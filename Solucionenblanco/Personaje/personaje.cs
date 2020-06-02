c4370a9a1bc93d7e804b13ef382e6470156512dd
Carlos-Lazo <carlos.lazo550@gmail.com>
2020-06-02 16:10:38 -0300

    Primer commit

---
+++ Solucionenblanco/Personaje/personaje.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Personajes
+{
+    class personaje
+    {
+        private int velocidad;
+        private int destreza;
+        private int fuerza;
+        private int nivel;
+        private int armadura;
+
+        private string tipo;
+        private string nombre;
+        private string apodo;
+        private int fechaN;
+        private int edad;
+        private int salud;
+
+        public int Velocidad { get => velocidad; set => velocidad = value; }
+        public int Destreza { get => destreza; set => destreza = value; }
+        public int Fuerza { get => fuerza; set => fuerza = value; }
+        public int Nivel { get => nivel; set => nivel = value; }
+        public int Armadura { get => armadura; set => armadura = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apodo { get => apodo; set => apodo = value; }
+        public int FechaN { get => fechaN; set => fechaN = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public int Salud { get => salud; set => salud = value; }
+    }
+}
